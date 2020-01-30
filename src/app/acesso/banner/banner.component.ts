932b1868bdf9be67a0f626f3b161d3f00b36e9e8
cesar-machado <cesarmach19@gmail.com>
2020-01-30 12:33:30 -0300

    Implementando animação na tela de acesso

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+import { Imagem } from './imagem.model'
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+       opacity: 0 
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition( 'escondido <=> visivel', animate('1s ease-in') )
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  estado: string = 'visivel'
+  imagens: Imagem[] = [
+    { estado: 'visivel', url:'/assets/banner-acesso/img_1.png'},
+    { estado: 'escondido', url:'/assets/banner-acesso/img_2.png'},
+    { estado: 'escondido', url:'/assets/banner-acesso/img_3.png'},
+    { estado: 'escondido', url:'/assets/banner-acesso/img_4.png'},
+    { estado: 'escondido', url:'/assets/banner-acesso/img_5.png'},
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(),3000)
   }
+
+  logicaRotacao(): void {
+
+    let idx: number 
+
+    for(let i:number = 0; i <= 4; i++) {
+
+      if(this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+
+        idx = i === 4 ? 0 : i + 1
+
+        break
+      }
+    }
+    //Exibis a próxima imagem
+    this.imagens[idx].estado ='visivel'
+
+    setTimeout(() => this.logicaRotacao(),3000)
+  }
+
 
 }
